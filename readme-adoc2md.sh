bf331e4d00a7f7337903a8d52c72ca753a945ee8
gautaz <11046232+gautaz@users.noreply.github.com>
2022-01-27 10:00:12 +0100

    Fix README's admonition icons for github
    Also remove old README's markdown "rendering"

--- readme-adoc2md.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-REPODIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -it -u "$(id -u):$(id -g)" -v "$REPODIR:/documents/" asciidoctor/docker-asciidoctor asciidoctor -b docbook readme-source.adoc -o - | docker run -i -u "$(id -u):$(id -g)" -v "$REPODIR:/data/" pandoc/core -f docbook -t gfm -s --wrap=none -o README.md
